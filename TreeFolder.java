047e972ccf4d3f89b30caca831da5450b90dd865
Nimish Vashisht <nimishvashisht@live.in>
2016-07-03 07:48:27 +0530

    Update TreeFolder.java

--- TreeFolder.java
+++ TreeFolder.java
@@ -1,30 +1,18 @@
-/*    */ package org.neuroph.easyneurons;
-/*    */ 
-/*    */ import javax.swing.tree.DefaultMutableTreeNode;
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ public class TreeFolder
-/*    */   extends DefaultMutableTreeNode
-/*    */ {
-/*    */   private static final long serialVersionUID = 1L;
-/*    */   
-/*    */   public TreeFolder(String label)
-/*    */   {
-/* 18 */     super(label);
-/*    */   }
-/*    */   
-/*    */   public boolean isLeaf() {
-/* 22 */     return false;
-/*    */   }
-/*    */ }
+ package org.neuroph.easyneurons;
 
+ import javax.swing.tree.DefaultMutableTreeNode;
 
-/* Location:              C:\Users\Nimish\Downloads\Neuroph OCR - jHRT alpha 0.2\Neuroph OCR - Handwriting Recognition alpha 0.2.jar!\org\neuroph\easyneurons\TreeFolder.class
- * Java compiler version: 6 (50.0)
- * JD-Core Version:       0.7.1
- */
+ public class TreeFolder
+   extends DefaultMutableTreeNode
+ {
+
+  public TreeFolder(String label)
+  {
+    super(label);
+  }
+ 
+  public boolean isLeaf() {
+   return false;
+   }
+ }
+
